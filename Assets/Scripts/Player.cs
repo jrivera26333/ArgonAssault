760854d4e3ca5db0dbf49980d6544a5b417998e3
jrivera26333 <jrivera26333@gmail.com>
2020-03-30 01:35:46 -0500

    Added Y Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
 
     [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
     [Tooltip("In m")] [SerializeField] float xRange = 5f;
+    [Tooltip("In m")] [SerializeField] float yRange = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +19,29 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffSet = xThrow * xSpeed * Time.deltaTime;
-        float rawXpos = transform.localPosition.x + xOffSet;
+        ProcessTranslation();
+        ProcessRotation();
+    }
 
+    private void ProcessRotation()
+    {
+        transform.localRotation = Quaternion.Euler(-30f, 30f, 0f);
+    }
+
+    private void ProcessTranslation()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffSet = xThrow * xSpeed * Time.deltaTime;
+        float yOffSet = yThrow * xSpeed * Time.deltaTime;
+
+        float rawXpos = transform.localPosition.x + xOffSet;
         float clampedXPos = Mathf.Clamp(rawXpos, -xRange, xRange);
 
-        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+        float rawYpos = transform.localPosition.y + yOffSet;
+        float clampedYPos = Mathf.Clamp(rawYpos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
 }
