b0c5612e4765512620c42f3291322d4fbd086793
jrivera26333 <jrivera26333@gmail.com>
2020-03-30 01:12:03 -0500

    Added Horizontal Movement to Ship

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
 {
 
     [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
+    [Tooltip("In m")] [SerializeField] float xRange = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +20,10 @@ public class Player : MonoBehaviour
     {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffSet = xThrow * xSpeed * Time.deltaTime;
+        float rawXpos = transform.localPosition.x + xOffSet;
+
+        float clampedXPos = Mathf.Clamp(rawXpos, -xRange, xRange);
+
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
     }
 }
