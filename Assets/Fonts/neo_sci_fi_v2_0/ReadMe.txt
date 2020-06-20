0632efd9bf73608f409195528f36555e03dd444d
jrivera26333 <jrivera26333@gmail.com>
2020-06-19 19:14:33 -0500

    Added Scoring Logic and UI Text

---
+++ Assets/Fonts/neo_sci_fi_v2_0/ReadMe.txt
@@ -0,0 +1,6 @@
+﻿///FONT NEO SCI FI V 2.O///
+////////////////////////////////////////////////
+
+Creator: BryAlien / BryanGu
+Installing: Windows XP: GO TO C>WINDOWS>FONTS AND PASTE THE FONT.
+------------  Windows Vista/7: Click with the right button of the mouse in font and click in Install.
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,18 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 12;
 
     BoxCollider enemyBoxCollider;
+    ScoreBoard scoreBoard;
 
     // Start is called before the first frame update
     void Start()
     {
         AddNonTriggerBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
     }
 
     private void AddNonTriggerBoxCollider()
@@ -24,6 +28,7 @@ public class Enemy : MonoBehaviour
 
     private void OnParticleCollision(GameObject other)
     {
+        scoreBoard.ScoreHit(scorePerHit);
         print("Particles collided with enemy!" + gameObject.name);
         GameObject fx = Instantiate(deathFX,transform.position, Quaternion.identity); //We are putting in the position since we are instantiating the particle
         fx.transform.parent = parent;
